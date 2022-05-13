0019dd4a5dbc59df36515fa1240bb44257857661
Bredon <arileo.pereira@gmail.com>
2022-05-13 09:53:20 +0200

    explanation

---
+++ src/components/NavBar.js/NavbarCom.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import { Container, Navbar } from 'react-bootstrap';
+
+const NavbarCom = () => {
+    return (
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container">
+                <a class="navbar-brand" href="#">Contacts</a>
+            </div>
+        </nav>
+
+    )
+}
+
+export default NavbarCom

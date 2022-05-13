a5724c8d6576c599207f065ef0ed2c932fe8fda9
Bredon <arileo.pereira@gmail.com>
2022-05-13 11:13:37 +0200

    problemsresolved2.0

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import { Navbar } from 'react-bootstrap';
 import './App.css';
 import Home from './Components/Home';
 
+
 function App() {
   return (
-    <div className="App">
+    <div>
       <Home />
     </div>
   );
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,20 +1,19 @@
-import { Axios } from 'axios'
+import Axios from 'axios'
 import React from 'react'
-import { Button } from 'react-bootstrap';
 
-const Home = () => {
-    // function to get the joke
+
+function Home() {
+
     const getJoke = () => {
-        Axios.Get("https://api.chucknorris.io/jokes/random").then((response) => {
-        console.log(response)
+        Axios.get("https://api.chucknorris.io/jokes/random").then((response) => {
+            console.log(response)
         })
     }
-    return (
-        <div>Salut
-            <button onClick={getJoke}>Blague</button>
-        </div>
-
-    )
+  return (
+    <div>Wassup
+        <button onClick={getJoke}>Click For the Joke</button>
+    </div>
+  )
 }
 
 export default Home

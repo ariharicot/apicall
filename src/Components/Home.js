e6f4f408ea084f8980e757bd3a26cebf2cac135c
Bredon <arileo.pereira@gmail.com>
2022-05-16 09:21:51 +0200

    problemssolvedappfin

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,21 +1,21 @@
 import Axios from 'axios'
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import './index.css';
 
 
 function Home() {
 
-    //const [affirmation, setJoke] = useState("")
+    const [joke, setJoke] = useState("")
 
     const getJoke = () => {
-        Axios.get("https://www.affirmations.dev/").then((response) => {
-            console.log(response);
-            //setJoke(response.data.affirmation)
+        fetch("https://api.chucknorris.io/jokes/random").then((response) => response.json()).then((data)=> {
+          setJoke(data.value);
         });
     }
   return (
-    <div>
-        <button onClick={getJoke}>Click For the Affirmation</button>
-
+    <div className='main-content'>
+        <button onClick={getJoke}>Click For the Joke</button>
+        {joke}
     </div>
   )
 }
